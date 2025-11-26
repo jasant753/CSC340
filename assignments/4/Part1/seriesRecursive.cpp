af8a2d972b7b8eeec9de90e3fedd173270dba9ec
jasant753 <jasant753@gmail.com>
2025-11-25 22:17:21 -0800

    Part 1 seriesRecursive code updated

--- assignments/4/Part1/seriesRecursive.cpp
+++ assignments/4/Part1/seriesRecursive.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
-#include <cstdlib> 
+#include <cstdlib>
 
 //TO DO: add your documentation here
 // TO DO: implement the recursive series function
 //      Hint: it should return a long long (integer)
+using namespace std;
 
+long long seriesRecursive(int n) {
+    if (n == 0) {
+        return n;
+    } else if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        return seriesRecursive(n - 1) + seriesRecursive(n - 2) + 4 * seriesRecursive(n-3);
+    }
+}
 
 int main(int argc, char* argv[]) {
     // Read n from command line argument
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
-        return 1; 
+        return 1;
     }
 
     // Convert n to an integer
-    int n = std::atoi(argv[1]); 
+    int n = std::atoi(argv[1]);
 
     // Compute series value of n
-    long long result = seriesRecursive(n); 
+    long long result = seriesRecursive(n);
     std::cout << "seriesRecursive(" << n << ") = " << result << std::endl;
 
-    return 0; 
+    return 0;
 }
